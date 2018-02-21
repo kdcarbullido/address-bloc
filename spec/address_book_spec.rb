81dd6912c2a3da12499d95fd7fd5b49716584f9e
Ken Carbullido <kdcarbullido@gmail.com>
2018-02-21 13:34:33 -0600

    Add tests for second file, entires_2.csv

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -92,4 +92,30 @@ RSpec.describe AddressBook do
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com" )
     end
   end
+
+  describe "#import_from_csv_2" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eq 3
+    end
+
+    it "imports csv2 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Samuel", "555-555-1111", "samuel@blocmail.com" )
+    end
+
+    it "imports csv2 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Tammy", "555-555-2222", "tammy@blocmail.com" )
+    end
+
+    it "imports csv2 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Victor", "555-555-3333", "victor@blocmail.com" )
+    end
+  end
 end
